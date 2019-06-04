106b86c6b5e842ad67377b4b1a7358c39ca3bb45
Jesse Tabak <jessejunior@hotmail.nl>
2019-06-04 11:15:56 +0200

    charts gelukt

--- SmartCity/SmartCity/MainDash.xaml.cs
+++ SmartCity/SmartCity/MainDash.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
 
 namespace SmartCity
 {
@@ -19,9 +21,39 @@ namespace SmartCity
     /// </summary>
     public partial class MainDash : Window
     {
+       
         public MainDash()
         {
             InitializeComponent();
+
+            SeriesCollection = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Title = "2015",
+                    Values = new ChartValues<int> { 10, 50, 39, 50 }
+                }
+            };
+
+            //adding series will update and animate the chart automatically
+            SeriesCollection.Add(new ColumnSeries
+            {
+                Title = "2016",
+                Values = new ChartValues<int> { 11, 56, 42 }
+            });
+
+            //also adding values updates and animates the chart automatically
+            SeriesCollection[1].Values.Add(48d);
+
+            Labels = new[] { "Maria", "Susan", "Charles", "Frida" };
+            Formatter = value => value.ToString("N");
+
+            DataContext = this;
         }
+
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> Formatter { get; set; }
+
     }
 }
